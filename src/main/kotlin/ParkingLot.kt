94b745906e2d44b8d8b679ebf8833947a75de0b6
baidyanath-p <baidyanath.p@homelane.com>
2020-01-25 23:07:22 +0530

    Input file read added

--- src/main/kotlin/ParkingLot.kt
+++ src/main/kotlin/ParkingLot.kt
@@ -1,3 +1,11 @@
-fun main(){
-    println("Hello world!!")
+import java.io.File
+
+fun main() {
+    // main method
+    val input = File("input.txt").readLines()
+
+    input.stream()
+        .forEach {
+            println(it)
+        }
 }
